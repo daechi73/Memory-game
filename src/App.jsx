09a3e10bd1de6f55fc38bc846b511a85cc8aa3f3
J <59581793+daechi73@users.noreply.github.com>
2023-08-11 23:34:26 -0400

    useEffect that fetches API and sets the data to the customCatDataList implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  const [customCatDataList, setCatData] = useState();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const getCats = (async () => {
+      const response = await fetch(
+        "https://api.giphy.com/v1/gifs/search?api_key=en7dMyIj3inRfEXKjj5pj9Hz0CjYPUx1&q=cat&limit=15&offset=0&rating=g&lang=en&bundle=messaging_non_clips",
+        { mode: "cors" }
+      );
+      const data = await response.json();
+      let catDataList = [];
+      data.data.forEach((catdata) => {
+        catDataList.push({
+          id: catdata.id,
+          name: catdata.title,
+          url: catdata.images.original.url,
+          selected: false,
+        });
+      });
+      catDataList = catDataList.map((catData) => {
+        return (
+          <Card url={catData.url} name={catData.name} key={catData.id} />
+          // <div key={catData.id}>
+          //   <img src={catData.url} alt="card" className="card" />
+          //   {catData.name}
+          // </div>
+        );
+      });
+      setCatData(catDataList);
+    })();
+  }, []);
+  console.log(customCatDataList);
+  console.log("hello");
+  return <>{customCatDataList}</>;
 }
 
-export default App
+export default App;
