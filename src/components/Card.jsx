40f00b1443dcccd040bb9afeb3981a06ca274b0c
J <59581793+daechi73@users.noreply.github.com>
2023-08-14 23:22:09 -0400

    added onClick event

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import "../css/Card.css";
 const Card = (props) => {
   return (
     <>
-      <div className="card" id={props.id}>
+      <div className="card" id={props.id} onClick={props.onClick}>
         <img className="card-img" src={props.url} alt={props.name} />
         <div className="catName">{props.name}</div>
       </div>
