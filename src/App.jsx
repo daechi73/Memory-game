156690337837dea892bbf9f15d20aa71bb2f918c
J <59581793+daechi73@users.noreply.github.com>
2023-08-14 23:20:05 -0400

    fixed url, added onClick and selected props to Card component, added a title and a container for cards as a return

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+import "./css/App.css";
 import Card from "./components/Card";
 
 function App() {
   const [score, setScore] = useState(0);
   const [customCatDataList, setCatData] = useState();
-
+  const handleClick = () => {
+    console.log("working");
+  };
   useEffect(() => {
     const getCats = (async () => {
       const response = await fetch(
-        "https://api.giphy.com/v1/gifs/search?api_key=en7dMyIj3inRfEXKjj5pj9Hz0CjYPUx1&q=cat&limit=15&offset=0&rating=g&lang=en&bundle=messaging_non_clips",
+        "https://api.giphy.com/v1/gifs/search?api_key=en7dMyIj3inRfEXKjj5pj9Hz0CjYPUx1&q=cat&limit=16&offset=0&rating=g&lang=en&bundle=messaging_non_clips",
         { mode: "cors" }
       );
       const data = await response.json();
@@ -24,19 +26,26 @@ function App() {
       });
       catDataList = catDataList.map((catData) => {
         return (
-          <Card url={catData.url} name={catData.name} key={catData.id} />
-          // <div key={catData.id}>
-          //   <img src={catData.url} alt="card" className="card" />
-          //   {catData.name}
-          // </div>
+          <Card
+            url={catData.url}
+            name={catData.name}
+            key={catData.id}
+            onClick={handleClick}
+            selected={catData.selected}
+          />
         );
       });
       setCatData(catDataList);
     })();
   }, []);
-  console.log(customCatDataList);
-  console.log("hello");
-  return <>{customCatDataList}</>;
+  // console.log(customCatDataList);
+  // console.log("hello");
+  return (
+    <>
+      <header className="game-title">Memory Game</header>
+      <div className="cards">{customCatDataList}</div>
+    </>
+  );
 }
 
 export default App;
