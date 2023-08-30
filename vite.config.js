1329036acb60d6cb091f6d7a24546c33c4dfc9f8
J <59581793+daechi73@users.noreply.github.com>
2023-08-29 20:21:57 -0400

    reordered defineconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Memory-game",
   plugins: [react()],
-  repo: "/Memory-game",
 });
